8dc8be31cc513f920e01d9c30909f19ec84b929c
MelipeFoon <felipe.t.moon@gmail.com>
2025-09-16 16:53:48 -0300

    Teste para o MissingOverride

---
+++ src/main/java/br/com/melipefoon/teste_api_livros/model/Pessoa.java
@@ -0,0 +1,30 @@
+package br.com.melipefoon.teste_api_livros.model;
+
+import lombok.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+//construtor padrão
+@NoArgsConstructor
+//construtor com todos os parâmetros, na ordem de declaração
+@AllArgsConstructor
+//quando aplicados a uma classe 
+//geram getters/setters para todos os campos não marcados como static
+@Getter
+@Setter
+@Entity
+public class Pessoa {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private int idade;
+
+    public String toString(){
+        return String.format("nome: %s/nsobrenome: %s/nidade: %d", nome, sobrenome, idade);
+    }
+}
