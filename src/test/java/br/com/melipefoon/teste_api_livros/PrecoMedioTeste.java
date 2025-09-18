1940612ae624fcedc32e79603979af7be860bef4
MelipeFoon <felipe.t.moon@gmail.com>
2025-09-18 20:37:34 -0300

    lib AutoConfigureMockMvc adicionada

--- src/test/java/br/com/melipefoon/teste_api_livros/PrecoMedioTeste.java
+++ src/test/java/br/com/melipefoon/teste_api_livros/PrecoMedioTeste.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
