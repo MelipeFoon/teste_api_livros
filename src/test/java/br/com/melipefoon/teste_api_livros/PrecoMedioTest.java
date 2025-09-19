dc3b1f5b17b84851379e63407596f8264cef513e
MelipeFoon <felipe.t.moon@gmail.com>
2025-09-18 21:31:02 -0300

    vaviowa

--- src/test/java/br/com/melipefoon/teste_api_livros/PrecoMedioTest.java
+++ src/test/java/br/com/melipefoon/teste_api_livros/PrecoMedioTest.java
@@ -25,7 +25,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 public class PrecoMedioTest {
-
     //para simular as requisições
     @Autowired
     private MockMvc mockMvc;
