a5b66d1db004b432ae275671b31cdf92e0c104e2
MelipeFoon <felipe.t.moon@gmail.com>
2025-11-05 21:41:46 -0300

    teste

--- src/main/java/br/com/melipefoon/teste_api_livros/repository/LivroRepository.java
+++ src/main/java/br/com/melipefoon/teste_api_livros/repository/LivroRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LivroRepository extends JpaRepository <Livro, Long> {
-}
+public interface LivroRepository extends JpaRepository <Livro, Long> {}
